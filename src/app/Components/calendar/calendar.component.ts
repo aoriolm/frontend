76c52e4e7cdf6b307e7778f5c0ea52f9af178902
aoriolm <77518092+aoriolm@users.noreply.github.com>
2022-06-13 15:23:39 +0200

    Update calendar.component.ts

--- src/app/Components/calendar/calendar.component.ts
+++ src/app/Components/calendar/calendar.component.ts
@@ -38,7 +38,7 @@ export class CalendarComponent implements OnInit {
         listMonth: { buttonText: 'Mes lista' },
       },
       editable: false,
-      timezone: 'FNT',
+      timezone: 'Europe/London',
     };
   }
 
